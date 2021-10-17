40b788d6383b3ecd29add1649dc3d1aa8edd63ab
JuanM134 <69129238+JuanM134@users.noreply.github.com>
2021-10-17 16:54:17 -0400

    Update hipo.php

--- hipo.php
+++ hipo.php
@@ -26,30 +26,37 @@
                         if(is_numeric($cateto1) && is_numeric($cateto2)){
 
                             $hipotenusa = sqrt (pow($cateto1,2) + pow($cateto2,2));
+                            echo "<br/><br/>";
                             echo "<h2 align='center'>El resultado es: " . $hipotenusa . "</h2>";
-                            echo "<a href='index.html'>Volver al atrás</a>";
+                            echo "<br/><br/>";
+                            echo "<a href='index.html'> <-- Volver a atrás</a>";
                         
 
                         }else{
 
-                        echo "El valor del Cateto 1 o del Cateto 2 deben ser númericos";
-                        echo "<a href='index.html'>Volver al atrás</a>";
+                        echo "<br/><br/>";
+                        echo "<h2 align='center'>El valor del Cateto 1 y del Cateto 2 deben ser númericos </h2>";
+                        echo "<br/><br/>";
+                        echo "<a href='index.html'> <-- Volver a atrás</a>";
 
 
                     }
 
                     }else{
-
-                        echo "El valor del Cateto 1 o del Cateto 2 no deben estar vacíos, 
-                        por favor añada un valor";
-                        echo "<a href='index.html'>Volver al atrás</a>";
+                        
+                        echo "<br/><br/>";
+                        echo "<h2 align='center'>El valor del Cateto 1 y del Cateto 2 no deben estar vacíos, 
+                        por favor añada un valor </h2>";
+                        echo "<br/><br/>";
+                        echo "<a href='index.html'> <-- Volver a atrás</a>";
 
                     }
 
                 }else{
 
+                    echo "<br/><br/>";
                     echo "Debe pulsar el botón de calcular";
-                    echo "<a href='index.html'>Volver al atrás</a>";
+                    echo "<a href='index.html'> <-- Volver a atrás</a>";
 
                 }
 
@@ -59,3 +66,4 @@
     </body>
 
 </html>
+
